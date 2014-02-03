767f77a2cf77ac042bff798fbd1ae432fb7438c3
Dean <dean@dean.(none)>
2014-02-02 20:39:41 -0500

    Added jamedy logo and watch page

---
+++ application/controllers/VideoController.php
@@ -0,0 +1,39 @@
+<?php
+
+class VideoController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function searchAction()
+    {
+        // action body
+    }
+
+    public function browseAction()
+    {
+        // action body
+    }
+
+    public function watchAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
